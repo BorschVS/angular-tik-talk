1678e6bebb6ba5420e349cf80e2d6533fa538df9
BorschVS <borschvadym@gmail.com>
2024-11-22 14:23:25 +0200

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProfileCardComponent } from './commom-ui/profile-card/profile-card.component';
-import { ProfileService } from './data/services/profile.service';
-import { JsonPipe } from '@angular/common';
-import { Profile } from './data/services/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCardComponent, JsonPipe],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  profileService = inject(ProfileService);
-  profiles: Profile[] = [];
   title = 'tik-talk';
-
-  constructor() {
-    this.profileService.getTestAccounts().subscribe((value) => {
-      this.profiles = value;
-    });
-  }
 }
--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,9 +1,10 @@
 import { Profile } from '../../data/services/interfaces/profile.interface';
 import { Component, Input } from '@angular/core';
+import { ImgUrlPipe } from '../../helpers/pipes/img-url.pipe';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [ImgUrlPipe],
   templateUrl: './profile-card.component.html',
   styleUrl: './profile-card.component.scss',
 })
