bd88d97333e31cea01c42832fb7346867310b6f0
BorschVS <borschvs.dev@gmail.com>
2024-11-23 21:38:12 +0200

    add auth and RxJS test methods

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../auth/auth.service';
+import { delay, from, map, skip, take, tap } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
+  authService = inject(AuthService);
 
+  form: FormGroup = new FormGroup({
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required),
+  });
+
+  constructor() {
+    // RxJS have "conveyor principle"
+    from([1, 2, 3, 4, 5, 6, 7, 8, 9])
+      .pipe(
+        map((val) => val * 2),
+        take(6),
+        skip(2),
+        delay(1000),
+        tap((val) => this.form.patchValue({ username: val.toString() }))
+      )
+      .subscribe((val) => {
+        console.log(val);
+      });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.authService.login(this.form.value).subscribe((res) => {
+        console.log(res);
+      });
+    }
+  }
 }
