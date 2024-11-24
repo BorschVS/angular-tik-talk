57eed25755555bdf289bf3411d08d7367db76a3c
BorschVS <borschvs.dev@gmail.com>
2024-11-24 16:15:39 +0200

    add auth token, refresh, signal, password visibility

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal, WritableSignal } from '@angular/core';
 import {
   FormControl,
   FormGroup,
@@ -6,40 +6,32 @@ import {
   Validators,
 } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
-import { delay, from, map, skip, take, tap } from 'rxjs';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-page',
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
 export class LoginPageComponent {
   authService = inject(AuthService);
+  router = inject(Router);
+
+  isPasswordVisible: WritableSignal<boolean> = signal<boolean>(false);
 
   form: FormGroup = new FormGroup({
     username: new FormControl<string | null>(null, Validators.required),
     password: new FormControl<string | null>(null, Validators.required),
   });
 
-  constructor() {
-    // RxJS have "conveyor principle"
-    from([1, 2, 3, 4, 5, 6, 7, 8, 9])
-      .pipe(
-        map((val) => val * 2),
-        take(6),
-        skip(2),
-        delay(1000),
-        tap((val) => this.form.patchValue({ username: val.toString() }))
-      )
-      .subscribe((val) => {
-        console.log(val);
-      });
-  }
+  constructor() {}
 
   onSubmit() {
     if (this.form.valid) {
       this.authService.login(this.form.value).subscribe((res) => {
+        this.router.navigate(['']);
         console.log(res);
       });
     }
